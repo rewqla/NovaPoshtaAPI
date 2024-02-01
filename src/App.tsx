08d28fbea02811b6e14c7a8d1a8dfd4a0b0cdf78
rewqla <desares526@gmail.com>
2024-02-01 18:07:27 +0200

    Add city selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,50 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import DatalistInput from "react-datalist-input";
+import "react-datalist-input/dist/styles.css";
 
 function App() {
   const API_URL = "https://api.novaposhta.ua/v2.0/json/";
   const apiKey = process.env.REACT_APP_NOVA_POSHTA_API_KEY;
 
-  const [responseData, setResponseData] = useState(null);
+  const [responseData, setResponseData] = useState([]);
 
-  const fetchData = async () => {
+  const getSettlements = async (value: string) => {
     try {
       const requestBody = {
-        apiKey: process.env.REACT_APP_NOVA_POSHTA_API_KEY,
+        apiKey: apiKey,
         modelName: "Address",
         calledMethod: "searchSettlements",
         methodProperties: {
-          CityName: "ки",
+          CityName: value,
           Limit: "10",
           Page: "1",
         },
       };
 
       const response = await axios.post(API_URL, requestBody);
-      setResponseData(response.data);
+      setResponseData(response.data.data[0].Addresses);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const handleCityChange = (event: any) => {
+    const value = event.target.value;
+    getSettlements(value);
+  };
 
   return (
     <>
-      <h1>HI</h1>
-      {responseData && (
-        <div>
-          <h2>Response Data:</h2>
-          <pre>{JSON.stringify(responseData, null, 2)}</pre>
-        </div>
-      )}
+      <DatalistInput
+        placeholder="Місто"
+        label="Виберіть населений пункт"
+        onChange={handleCityChange}
+        onSelect={(item) => console.log(item.value)}
+        items={responseData.map((city: any) => ({
+          id: city.Ref,
+          value: city.Present,
+        }))}
+      />
     </>
   );
 }
