80f36b31f7b0c7ab4fead16702ec1789675a3c20
rewqla <desares526@gmail.com>
2024-02-01 20:47:09 +0200

    Add a warehouse selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import DatalistInput from "react-datalist-input";
 import "react-datalist-input/dist/styles.css";
 
@@ -8,6 +8,8 @@ function App() {
   const apiKey = process.env.REACT_APP_NOVA_POSHTA_API_KEY;
 
   const [responseData, setResponseData] = useState([]);
+  const [warehouses, setWarehouses] = useState([]);
+  const [selectedCityRef, setSelectedCityRef] = useState("");
 
   const getSettlements = async (value: string) => {
     try {
@@ -24,10 +26,39 @@ function App() {
 
       const response = await axios.post(API_URL, requestBody);
       setResponseData(response.data.data[0].Addresses);
+      console.log(responseData);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
+
+  const getWarehouses = async (value: string) => {
+    try {
+      console.log(value);
+
+      const requestBody = {
+        apiKey: apiKey,
+        modelName: "Address",
+        calledMethod: "getWarehouses",
+        methodProperties: {
+          CityRef: value,
+          Page: "1",
+          Limit: "100",
+          Language: "UA",
+        },
+      };
+
+      const response = await axios.post(API_URL, requestBody);
+      setWarehouses(response.data.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    getWarehouses(selectedCityRef);
+  }, [selectedCityRef]);
+
   const handleCityChange = (event: any) => {
     const value = event.target.value;
     getSettlements(value);
@@ -39,10 +70,22 @@ function App() {
         placeholder="Місто"
         label="Виберіть населений пункт"
         onChange={handleCityChange}
-        onSelect={(item) => console.log(item.value)}
+        onSelect={async (item) => {
+          setSelectedCityRef(item.id);
+        }}
         items={responseData.map((city: any) => ({
-          id: city.Ref,
+          id: city.DeliveryCity,
           value: city.Present,
+        }))}
+      />
+      <DatalistInput
+        placeholder="Номер"
+        label="Виберіть точку видачі"
+        onChange={handleCityChange}
+        onSelect={(item) => setSelectedCityRef(item.id)}
+        items={warehouses.map((warehouse: any) => ({
+          id: warehouse.Ref,
+          value: warehouse.Description,
         }))}
       />
     </>
